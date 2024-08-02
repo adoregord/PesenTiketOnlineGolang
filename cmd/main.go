fbcfd1bc953bd0439180175ffd92f500cdbf7d67
adoregord <fadhlideede3@gmail.com>
2024-08-02 13:32:07 +0700

    feat: simplify main and set the routes and group them so its easier to read in routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,77 +1,27 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"net/http"
-	"pemesananTiketOnlineGo/internal/domain"
-	"pemesananTiketOnlineGo/internal/handler"
-	"pemesananTiketOnlineGo/internal/repository"
-	"pemesananTiketOnlineGo/internal/usecase"
+	"pemesananTiketOnlineGo/internal/provider/db"
+	"pemesananTiketOnlineGo/internal/provider/db/routes"
 	"runtime"
 	"sync"
+
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	runtime.GOMAXPROCS(4)
 	var wg sync.WaitGroup
 
-	// event connection
-	eventRepo := repository.NewEventRepo()
-	eventUsecase := usecase.NewEventUsecase(eventRepo)
-	eventHandler := handler.NewEventHandler(eventUsecase)
-
-	// user connection
-	userRepo := repository.NewUserRepo()
-	userUsecase := usecase.NewUserUsecase(userRepo)
-	userHandler := handler.NewUserHandler(userUsecase)
-
-	// order connection
-	orderRepo := repository.NewOrderRepo()
-	orderUsecase := usecase.NewOrderUsecase(orderRepo, eventRepo, userRepo)
-	orderHandler := handler.NewOrderHandler(orderUsecase)
-
-	// create event
-
-	tickets := []domain.Ticket{
-		{ID: 1, Type: "VIP", Price: 5000.0, Quantity: 10},
-		{ID: 2, Type: "CAT 1", Price: 250.0, Quantity: 100},
+	// inisialisasi koneksi ke database
+	database, err := db.DBConnection()
+	if err != nil {
+		log.Error().Str("Error: ", err.Error())
 	}
+	defer database.Close()
 
-	events := []domain.Event{
-		{ID: 1, Name: "Concert1", Date: "02-Jan-2006 15:04:05", Description: "Awokwok1", Location: "Location1", Ticket: tickets},
-		{ID: 2, Name: "Concert2", Date: "03-Jan-2006 15:04:05", Description: "Awokwok2", Location: "Location2", Ticket: tickets},
-		{ID: 3, Name: "Concert3", Date: "04-Jan-2006 15:04:05", Description: "Awokwok3", Location: "Location3", Ticket: tickets},
-		{ID: 4, Name: "Concert4", Date: "03-Jan-2006 15:04:05", Description: "Awokwok4", Location: "Location4", Ticket: tickets},
-		{ID: 5, Name: "Concert5", Date: "03-Jan-2006 15:04:05", Description: "Awokwok5", Location: "Location5", Ticket: tickets},
-	}
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for _, value := range events {
-			eventUsecase.CreateEvent(value, context.Background())
-		}
-	}()
-
-	routes := http.NewServeMux()
-	routes.HandleFunc("/event", eventHandler.CreateEvent)
-	routes.HandleFunc("/eventGet", eventHandler.GetAllEvents) //check all ticket and event
-	routes.HandleFunc("/eventGetById", eventHandler.GetEventByID)
-	routes.HandleFunc("/eventGetByName", eventHandler.GetEventByName)
-	routes.HandleFunc("/eventUpdate", eventHandler.UpdateEvent)
-	routes.HandleFunc("/eventDelete", eventHandler.DeleteEvent)
-
-	routes.HandleFunc("/userPost", userHandler.CreateUser)
-	routes.HandleFunc("/userGetAll", userHandler.GetAllUsers)
-	routes.HandleFunc("/userGetById", userHandler.GetUserByID)
-	routes.HandleFunc("/userGetByName", userHandler.GetUserByName)
-	routes.HandleFunc("/userUpdate", userHandler.UpdateUser)
-	routes.HandleFunc("/userDelete", userHandler.DeleteUser)
-
-	routes.HandleFunc("/buyTicket", orderHandler.CreateOrder) // buy the ticket
-	routes.HandleFunc("/orderGetAll", orderHandler.GetAllOrders)
-	routes.HandleFunc("/orderGetByUserId", orderHandler.GetOrderByID) // list all orders from that one user
+	routes := routes.SetupRoutes(database)
 
 	server := http.Server{}
 	server.Handler = routes
@@ -80,11 +30,79 @@ func main() {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		fmt.Println("Server berjalan di http://localhost:8080")
+		log.Info().Msg("Server berjalan di http://localhost:8080")
 		if err := server.ListenAndServe(); err != nil {
-			fmt.Println("Error starting server:", err)
+			log.Error().Str("Error starting server:", err.Error())
 		}
 	}()
 
 	wg.Wait()
+	// // ticket repo
+	// ticketRepo := repository.NewTicketRepo(database)
+
+	// // event connection
+	// eventRepo := repository.NewEventRepo(database)
+	// eventUsecase := usecase.NewEventUsecase(eventRepo, ticketRepo)
+	// eventHandler := handler.NewEventHandler(eventUsecase)
+
+	// // user connection
+	// userRepo := repository.NewUserRepo(database)
+	// userUsecase := usecase.NewUserUsecase(userRepo)
+	// userHandler := handler.NewUserHandler(userUsecase)
+
+	// // order connection
+	// orderRepo := repository.NewOrderRepo(database)
+	// orderUsecase := usecase.NewOrderUsecase(orderRepo, eventRepo, userRepo)
+	// orderHandler := handler.NewOrderHandler(orderUsecase)
+
+	// create event
+
+	// tickets := []domain.Ticket{
+	// 	{ID: 1, Type: "VIP", Price: 5000.0, Quantity: 10},
+	// 	{ID: 2, Type: "CAT 1", Price: 250.0, Quantity: 100},
+	// }
+
+	// events := []domain.Event{
+	// 	{ID: 1, Name: "Concert1", Date: "02-Jan-2006 15:04:05", Description: "Awokwok1", Location: "Location1", Ticket: tickets},
+	// 	{ID: 2, Name: "Concert2", Date: "03-Jan-2006 15:04:05", Description: "Awokwok2", Location: "Location2", Ticket: tickets},
+	// 	{ID: 3, Name: "Concert3", Date: "04-Jan-2006 15:04:05", Description: "Awokwok3", Location: "Location3", Ticket: tickets},
+	// 	{ID: 4, Name: "Concert4", Date: "03-Jan-2006 15:04:05", Description: "Awokwok4", Location: "Location4", Ticket: tickets},
+	// 	{ID: 5, Name: "Concert5", Date: "03-Jan-2006 15:04:05", Description: "Awokwok5", Location: "Location5", Ticket: tickets},
+	// }
+
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	for _, value := range events {
+	// 		eventUsecase.CreateEvent(value, context.Background())
+	// 	}
+	// }()
+
+	// routes := http.NewServeMux()
+	// routes.HandleFunc("/event", eventHandler.CreateEvent)
+	// routes.HandleFunc("/eventGet", eventHandler.GetAllEvents) //check all ticket and event
+	// routes.HandleFunc("/eventGetById", eventHandler.GetEventByID)
+	// routes.HandleFunc("/eventGetByName", eventHandler.GetEventByName)
+	// routes.HandleFunc("/eventUpdate", eventHandler.UpdateEvent)
+	// routes.HandleFunc("/eventDelete", eventHandler.DeleteEvent)
+
+	// routes.HandleFunc("/eventCreateDB", eventHandler.CreateEventDB) // create ticket to db
+	// routes.HandleFunc("/eventGetAllDB", eventHandler.ViewAllEvents) //check all ticket and event from db
+	// routes.HandleFunc("/eventGetID", eventHandler.ViewEventByIdDB)  //check ticket by event id from db
+
+	// routes.HandleFunc("/userPost", userHandler.CreateUser)
+	// routes.HandleFunc("/userGetAll", userHandler.GetAllUsers)
+	// routes.HandleFunc("/userGetById", userHandler.GetUserByID)
+	// routes.HandleFunc("/userGetByName", userHandler.GetUserByName)
+	// routes.HandleFunc("/userUpdate", userHandler.UpdateUser)
+	// routes.HandleFunc("/userDelete", userHandler.DeleteUser)
+
+	// routes.HandleFunc("/userPostDB", userHandler.CreateUserDB)   // post user to db
+	// routes.HandleFunc("/userGetAllDB", userHandler.GetAllUserDB) // view user from db
+	// routes.HandleFunc("/userGetIDDB", userHandler.GetUserIDDB)   // view user with ID from db
+
+	// routes.HandleFunc("/buyTicket", orderHandler.CreateOrder)     // buy the ticket
+	// routes.HandleFunc("/buyTicketDB", orderHandler.CreateOrderDB) // buy the ticket
+	// routes.HandleFunc("/orderGetAll", orderHandler.GetAllOrders)
+	// routes.HandleFunc("/orderGetByUserId", orderHandler.GetOrderByID) // list all orders from that one user
 }
---
+++ internal/provider/db/routes/routes.go
@@ -0,0 +1,54 @@
+package routes
+
+import (
+	"database/sql"
+	"pemesananTiketOnlineGo/internal/handler2"
+	"pemesananTiketOnlineGo/internal/repository2"
+	"pemesananTiketOnlineGo/internal/usecase2"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(database *sql.DB) *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+
+	userRepo2 := repository2.NewUserRepo(database)
+	userUsecase2 := usecase2.NewUserUsecase(userRepo2)
+	userHandler2 := handler2.NewUserHandler(userUsecase2)
+
+	ticketRepo2 := repository2.NewTicketRepo(database)
+
+	eventRepo2 := repository2.NewEventRepo(database)
+	eventUsecase2 := usecase2.NewEventUsecase(eventRepo2, ticketRepo2)
+	eventHandler2 := handler2.NewEventHandler(eventUsecase2)
+
+	orderRepo2 := repository2.NewOrderRepo(database)
+	orderUsecase2 := usecase2.NewOrderUsecase(orderRepo2, eventRepo2, userRepo2)
+	orderHandler2 := handler2.NewOrderHandler(orderUsecase2)
+
+	userRoutes := router.Group("/user")
+	{
+		userRoutes.GET("/get", userHandler2.GetAllUserDB)
+		userRoutes.GET("/getByID", userHandler2.GetUserIDDB)
+		userRoutes.POST("/create", userHandler2.CreateUserDB)
+		userRoutes.PATCH("addBalance", userHandler2.AddBalanceDB)
+		userRoutes.DELETE("/delete", userHandler2.DeleteUserDB)
+	}
+
+	eventRoutes := router.Group("/event")
+	{
+		eventRoutes.GET("/get", eventHandler2.ViewAllEvents)
+		eventRoutes.GET("/getByID", eventHandler2.ViewEventByIdDB)
+		eventRoutes.POST("/create", eventHandler2.CreateEventDB)
+		eventRoutes.POST("/addTicket", eventHandler2.UpdateEventTicketDB)
+		eventRoutes.DELETE("/delete", eventHandler2.DeleteEventDB)
+	}
+
+	orderRoutes := router.Group("/order")
+	{
+		orderRoutes.POST("/create", orderHandler2.CreateOrderDB)
+	}
+
+	return router
+}
